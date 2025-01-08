b909c00693ffe77cb96e4759d78098efa1cb7fc6
fuhad saneen <saneenfuhad81@gmail.com>
2025-01-08 09:33:27 +0530

    Time: 2 ms (70.74%), Space: 51.4 MB (94.29%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let even=nums.filter(num=> num % 2===0)
+    let odd=nums.filter(num=> num%2!==0)
+    return even.concat(odd) 
+};
