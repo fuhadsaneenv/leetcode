64b55d04637966eada801dd4edbb5de640bd7773
fuhad saneen <saneenfuhad81@gmail.com>
2025-01-11 09:52:46 +0530

    Time: 1 ms (76.76%), Space: 49.2 MB (79.37%) - LeetHub

---
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
+ */
+var uncommonFromSentences = function(s1, s2) {
+    const words = (s1 + " " + s2).split(" ");
+    const wordCount = new Map();
+    words.forEach(word => {
+    wordCount.set(word, (wordCount.get(word) || 0) + 1);
+  });
+    return Array.from(wordCount.keys()).filter(word => wordCount.get(word) === 1)
+};
