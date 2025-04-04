912c91cc7ad8febb65447868d074bc1a0cf156bb
fuhad saneen <saneenfuhad81@gmail.com>
2025-04-04 09:27:49 +0530

    Time: 0 ms (100%), Space: 53.8 MB (54.8%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    return strs.reduce((a, b) => {
+    while (!b.startsWith(a)) a = a.slice(0, -1);
+    return a;
+  });  
+};
