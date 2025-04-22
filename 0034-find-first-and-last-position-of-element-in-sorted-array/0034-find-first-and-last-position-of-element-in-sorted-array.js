e9b0ec6af4756d51e3eb9282cc74fa34a9847efc
fuhad saneen <saneenfuhad81@gmail.com>
2025-04-22 08:59:16 +0530

    Time: 0 ms (100%), Space: 54.6 MB (54.61%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    // let res=[-1,-1]
+    // res[0]=lastIndexOf()
+    // res[1]=
+
+    return [nums.indexOf(target),nums.lastIndexOf(target)]
+    
+};
